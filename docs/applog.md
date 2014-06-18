01cbaba028db926571d453766fbb2ab130b57eca
Mateusz <matgrotha@gmail.com>
2014-06-18 15:09:14 +0200

    styling, fixes, docs

---
+++ docs/applog.md
@@ -0,0 +1,11 @@
+Progress log of Songbook app
+
+16.08.2014
+  Basic functionality, and pushing whole thing to github repo.
+
+18.08.2014
+   Added css styling (webkit and moz compatibile)
+   Fixed minor bugs
+   Added word limit to posts/songs title to 25
+   Added applog.md file in docs directory
+   Added screenshots in docs/commit_date directory
--- songbook.rb
+++ songbook.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require "sinatra/basic_auth"
 require 'data_mapper'
 require 'time'
- 
+
 enable :sessions
 set :session_secret, 'secret'
 
@@ -10,16 +10,16 @@ SITE_TITLE = "Songbook"
 SITE_DESCRIPTION = "a simple blogging platform"
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/songs.db")
- 
+
 class Song
   include DataMapper::Resource
   property :id, Serial
-  property :title_post, String, :required => true
+  property :title_post, String, :length => 25, :required => true
   property :content, Text, :required => true
   property :created_at, DateTime
   property :updated_at, DateTime
 end
- 
+
 DataMapper.finalize.auto_upgrade!
 
 helpers do
@@ -32,58 +32,58 @@ authorize "Admin" do |username, password|
 end
 
 get '/' do
-    @songs = Song.all :order => :id.desc
-    @title = 'All Posts'
-    erb :start
+  @songs = Song.all :order => :id.desc
+  @title = 'All Posts'
+  erb :start
 end
 
 protect "Admin" do
 	get '/panel' do
-		  @songs = Song.all :order => :id.desc
-		  @title = 'All Posts'
-		  erb :home
+	  @songs = Song.all :order => :id.desc
+	  @title = 'All Posts'
+	  erb :home
 	end
 
 	post '/panel' do
-		  s = Song.new
-		  s.content = params[:content]
-		  s.title_post = params[:title_post]
-		  s.created_at = Time.now
-		  s.updated_at = Time.now
-		  s.save
-		  redirect '/panel'
+    s = Song.new
+	  s.content = params[:content]
+	  s.title_post = params[:title_post]
+	  s.created_at = Time.now
+	  s.updated_at = Time.now
+	  s.save
+	  redirect '/panel'
 	end
 
 	get '/panel/:id' do
-		  @song = Song.get params[:id]
-		  @title = "Edit note ##{params[:id]}"
-		  if @song
-		      erb :edit
-		  else
-		      redirect '/panel'
-		  end
+	  @song = Song.get params[:id]
+	  @title = "Edit note ##{params[:id]}"
+	  if @song
+	      erb :edit
+	  else
+	      redirect '/panel'
+	  end
 	end
 
 	put '/panel/:id' do
-		  s = Song.get params[:id]
-		  s.content = params[:content]
-		  s.title_post = params[:title_post]
-		  s.updated_at = Time.now
-		  s.save
-		  redirect '/panel'    
+	  s = Song.get params[:id]
+	  s.content = params[:content]
+	  s.title_post = params[:title_post]
+	  s.updated_at = Time.now
+	  s.save
+	  redirect '/panel'
 	end
 
 	get '/panel/:id/delete' do
-		  @song = Song.get params[:id]
-		  @title = "Confirm deletion of post ##{params[:id]}"
-		  if @song
-		     erb :delete
-		  end
+	  @song = Song.get params[:id]
+	  @title = "Confirm deletion of post ##{params[:id]}"
+	  if @song
+	     erb :delete
+	  end
 	end
 
 	delete '/panel/:id' do
-		  s = Song.get params[:id]
-		  s.destroy
-		  redirect '/panel'
+	  s = Song.get params[:id]
+	  s.destroy
+	  redirect '/panel'
 	end
 end
